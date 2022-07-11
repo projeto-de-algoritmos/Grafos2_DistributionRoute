28f8ac2e79842c52d040510111b1a436b43ae6b5
Vitor Lamego <vitorlamegoo@gmail.com>
2022-07-11 16:47:15 -0300

    Arrumando navegação no mapa

--- lib/core.dart
+++ lib/core.dart
@@ -1,3 +1,12 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class Global {
-  String api_key = "AIzaSyCPDyusnf6v5acaMIUWfO1PvDWbT3WlhXo";
+  static List<Marker> markers = [
+    Marker(
+      markerId: MarkerId('Base'),
+      position: LatLng(-15.989557572245221, -48.044411838054664),
+      infoWindow: InfoWindow(title: 'UnB Gama'),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueOrange),
+    ),
+  ];
 }
--- lib/map.dart
+++ lib/map.dart
@@ -3,8 +3,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:grafos2_app/bottomSheet/bottomSheet.dart';
+import 'package:grafos2_app/controller.dart';
+import 'package:grafos2_app/core.dart';
 
 class MapSample extends StatefulWidget {
   @override
@@ -14,96 +16,53 @@ class MapSample extends StatefulWidget {
 class MapSampleState extends State<MapSample> {
   Completer<GoogleMapController> _controller = Completer();
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static final CameraPosition _kLake = CameraPosition(
+  static final CameraPosition _UnBGama = CameraPosition(
       bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
+      target: LatLng(-15.989557572245221, -48.044411838054664),
       zoom: 19.151926040649414);
-
-  List<Marker> markers = [
-    Marker(
-        markerId: MarkerId('Base'),
-        position: LatLng(37.42089449167863, -122.08532821387054),
-        infoWindow: InfoWindow(title: 'Central distribuidora'),
-        icon:
-            BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueOrange)),
-    Marker(
-      markerId: MarkerId('Base2'),
-      position: LatLng(37.42076588123734, -122.08291556686162),
-      infoWindow: InfoWindow(title: 'Central distribuidora'),
-      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueOrange),
-    )
-  ];
-
-  List<LatLng> polylineCoordinates = [];
-
-  void getPolyPoints() async {
-    PolylinePoints polylinePoints = PolylinePoints();
-    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
-        "AIzaSyCPDyusnf6v5acaMIUWfO1PvDWbT3WlhXo",
-        PointLatLng(37.42089449167863, -122.08532821387054),
-        PointLatLng(37.42076588123734, -122.08291556686162));
-
-    if (result.points.isNotEmpty) {
-      result.points.forEach((element) {
-        polylineCoordinates.add(LatLng(element.latitude, element.longitude));
-      });
-
-      setState(() {});
-    }
-  }
-
-  @override
-  void initState() {
-    getPolyPoints();
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
         mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
+        initialCameraPosition: _UnBGama,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
-        markers: Set<Marker>.of(markers),
-        onLongPress: (latLng) {
+        markers: Set<Marker>.of(Global.markers),
+        onLongPress: (latLng) async {
+          await showRegisterDialog(context, latLng);
           print('${latLng.latitude}, ${latLng.longitude}');
-          markers.add(
-            Marker(
-              markerId: MarkerId('${latLng.latitude}'),
-              position: LatLng(latLng.latitude, latLng.longitude),
-              infoWindow: InfoWindow(title: 'The title of the marker'),
-            ),
-          );
-
-          setState(() {});
-        },
-        polylines: {
-          Polyline(
-              polylineId: PolylineId("Route"),
-              points: polylineCoordinates,
-              color: Colors.red,
-              width: 6)
+          print(Controller.distanceBetweenPoints(
+              Global.markers[0], Global.markers[1]));
         },
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('Faça o caminho!'),
+        onPressed: nextStop,
+        label: Text('Próxima Parada!'),
         icon: Icon(Icons.directions_boat),
       ),
     );
   }
 
-  Future<void> _goToTheLake() async {
+  Future<void> nextStop() async {
     final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    CameraPosition nextStop = CameraPosition(
+        bearing: 192.8334901395799,
+        target: LatLng(0, 0),
+        tilt: 59.440717697143555,
+        zoom: 19.151926040649414);
+    controller.animateCamera(CameraUpdate.newCameraPosition(nextStop));
+  }
+
+  showRegisterDialog(BuildContext contextPage, LatLng latLng) {
+    return showBottomSheet(
+      backgroundColor: Colors.transparent,
+      context: context,
+      builder: (BuildContext context) {
+        return BottomSheetDialog();
+      },
+    );
   }
 }
